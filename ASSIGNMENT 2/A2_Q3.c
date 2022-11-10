fc90f5ea229d647ee5abdc2f0157ecd3db3af32d
DhaneshJoshi842 <115444628+DhaneshJoshi842@users.noreply.github.com>
2022-11-10 23:10:03 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/A2_Q3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// created by dhanesh on 24 sept.2022 to print value of a and b in different line
+int main()
+{
+    int a = 10, b = 20;
+    printf("value of a is %d\n", a);
+    printf("value of b is %d\n", b);
+
+    return 0;
+}
