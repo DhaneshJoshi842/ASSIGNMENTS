6cfe82a7e7f6c3023e1f97fc396f6a517b218c9d
DhaneshJoshi842 <115444628+DhaneshJoshi842@users.noreply.github.com>
2022-11-10 23:12:49 +0530

    Add files via upload

---
+++ ASIGNMENT 3/A3_Q1Q2Q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*creator-Dhanesh Joshi
+Date-25/9/22
+Aim-To interact program and user through scanf*/
+int main()
+{
+    int a;
+    int b;
+    printf("please enter value of a\n");
+    scanf("%d", &a);
+    printf("please enter value of b\n");
+    scanf("%d",&b);
+        printf("value of 1st variable is variable is %d and value of 2nd variable is %d",a,b);}
+
+    
