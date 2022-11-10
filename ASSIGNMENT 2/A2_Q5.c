1bcf0d31c637411d530d81494fdfbe97179f344c
DhaneshJoshi842 <115444628+DhaneshJoshi842@users.noreply.github.com>
2022-11-10 23:11:07 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/A2_Q5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// created by Dhanesh on 24 sept.2022 to
+int main()
+{
+    int a = 15, b = 16, c = 17, d = 18;
+    printf("a holds an integer constant %d\n", a);
+    printf("b holds an integer constant %d\n", b);
+    printf("c holds an integer constant %d\n", c);
+    printf("d holds an integer constant %d\n", d);
+    return 0;
+}
