d410e7a3e0df601edcf78cf3ede242b2bc1492fe
DhaneshJoshi842 <115444628+DhaneshJoshi842@users.noreply.github.com>
2022-11-10 23:23:01 +0530

    Add files via upload

---
+++ ASSIGNMENT 5/A5_Q2Q3Q4Q5Q6Q7.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/*creator-dhanesh joshi
+date-27/9/22
+aim-to find out some parameters of circle,square and rectangle*/
+void rectangle();
+void circle();
+void square();
+int main()
+{
+    printf("WELCOME\n");
+    rectangle();
+    circle();
+    square();
+    return 0;
+}
+void rectangle()
+{
+    int l, b, aofr, pofr;
+    printf("*To find out area and perimeter of rectangle-\n");
+    printf("please enter length\n");
+    scanf("%d", &l);
+    printf("please enter breadth\n");
+    scanf("%d", &b);
+    aofr = l * b;
+    pofr = 2 * (l + b);
+    printf("area of rectangle is %d\n", aofr);
+    printf("perimeter of rectangle is %d\n", pofr);
+}
+void circle()
+{
+    int r;
+    float pi = 3.14, cofc, aofc;
+    printf("*To find out circumference and area of circle-\n");
+    printf("please enter radious of circle\n");
+    scanf("%d", &r);
+    aofc = pi * r * r;
+    cofc = 2 * pi * r;
+    printf("area of circle is %f\n", aofc);
+    printf("circumference of circle is %f\n", cofc);
+}
+void square()
+{
+    int side, aofs, pofs;
+    printf("*To find out area and perimeter of square-\n");
+    printf("enter side of square\n");
+    scanf("%d", &side);
+    aofs = side * side;
+    pofs = 4 * side;
+    printf("area of square is %d\n", aofs);
+    printf("circumferemce of square is %d\n", pofs);
+}
---
+++ ASSIGNMENT 5/A5_Q8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//created by dhanesh joshi to find out the sum of three sides of triangle
+int main()
+{int a,b,c;
+printf("enter 1st side\n");
+scanf("%d",&a);
+printf("enter 2nd side\n");
+scanf("%d",&b);
+printf("enter 3rd side\n");
+scanf("%d",&c);
+printf("sum of the sides of the triangle is %d",a+b+c);
+
+
+
+}
