3c1c5b7109ec0d91f7d7936129b4a9eec484c918
DhaneshJoshi842 <115444628+DhaneshJoshi842@users.noreply.github.com>
2022-11-10 23:20:36 +0530

    Add files via upload

---
+++ ASSIGNMENT 4/A4_Q9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//created by dhanesh to convert feet into inches
+int main()
+{int a,b;
+printf("enter height of a student in feet\n");
+scanf("%d",&a);
+b=a*12;
+printf("height of a student in inches is %d",b);
+
+
+}
